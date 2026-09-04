7d05c2d3e513404f9c281a799c2c62aa4e3b2dc9
Tiina Marit Olsen Lahtinen <tiina.lahtine80@gmail.com>
2026-09-04 13:04:02 +0200

    Task 2.1

---
+++ module-1/example_01.mjs
@@ -0,0 +1,18 @@
+"use strict"
+import { printOut } from "../../common/script/utils.mjs";
+
+let a = true;
+let b = false;
+
+
+
+printOut ({a, b});
+
+const person =
+{
+    name: "Arne",
+    age: 25,
+    height:178
+};
+
+printOut( person );
